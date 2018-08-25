8dff3de3f689420b756bff0ae28e0ebcd50ea233
benoctopus <rose.benjamin925@gmail.com>
2018-08-25 11:59:37 -0700

    changed eslint, index.html, extracted login page from app, added theme object, fed the dog

--- src/client/App.js
+++ src/client/App.js
@@ -1,34 +1,63 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Login from './components/Login';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { deepPurple, purple } from '@material-ui/core/colors';
+import LoginPage from './components/containers/LoginPage';
+
+// example theme, please change
+// you can use hex colors too
+// you can also color theme components ie: primary, dard, contrastText ...
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: purple
+  }
+});
 
 const styles = {
-  App: {
-    position: 'relative',
+  app: {
+    height: '100vh',
+    width: '100vw',
+    overflow: 'hidden'
   },
-  AppHeader: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-  }
-};
-const App = () => {
-  return (
-    <div className="App">
-      <header className="AppHeader">
-        <h1 className="App-title"><i>Popitize</i></h1>
-      </header>
-      <Login classes={
-        {
-          card: 'signUpCard',
-          buttonContainer: 'buttonContainer',
-        }
-      }
-      />
-    </div>
-    )
 };
 
-const AppStyled = withStyles(styles)(App);
-export default AppStyled;
-// export default App;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    };
+  }
+
+  handleChangeIndex = (index) => {
+    if (index > 1 || index < 0) {
+      throw new Error(`Index ${index} of range`);
+    }
+    this.setState({ index });
+  }
+
+  render = () => {
+    const { classes } = this.props;
+    const { index } = this.state;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.app}>
+          {
+            index === 0 ?
+              <LoginPage handleChangeIndex={this.handleChangeIndex} />
+            :
+              <p>chat dashboard</p>
+          }
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+
+export default withStyles(styles)(App);
